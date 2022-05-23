68930d84f17e504445c041a860badda77eaa266e
jantapabh <jantapa2407@gmail.com>
2022-05-23 09:16:29 +0700

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,38 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { GA_TRACKING_ID } from "../lib/gtag";
+import { pageview } from '../lib/gtag';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  useEffect(() => {
+    const handleRouteChange = url => {
+      pageview(url, document.title);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, []);
+
+  return (
+    <>
+         <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${GA_TRACKING_ID}');
+              `,
+            }}
+          />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
